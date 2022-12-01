cb6f647bff72d8ae339480f3739c06b7f51e71d2
Uliana Gritchina <ugritchina@icloud.com>
2022-12-01 09:20:34 +0300

    Starter view. App icon.

--- CubeTimer/More/CubeTimerApp.swift
+++ CubeTimer/More/CubeTimerApp.swift
@@ -4,21 +4,7 @@ import SwiftUI
 struct CubeTimerApp: App {
     var body: some Scene {
         WindowGroup {
-            TabView {
-                TimerView()
-                    .tabItem {
-                        Label("Main", systemImage: "clock")
-                    }
-                ResultsView(deleteAction: {})
-                    .tabItem {
-                        Label("Results", systemImage: "list.dash")
-                    }
-                
-                Color.blue
-                    .tabItem {
-                        Label("Settings", systemImage: "gear")
-                    }
-            }
+            StarterView()
         }
     }
 }
---
+++ CubeTimer/Screens/Starter/StarterView.swift
@@ -0,0 +1,68 @@
+//
+//  StarterView.swift
+//  CubeTimer
+//
+//  Created by Ульяна Гритчина on 30.11.2022.
+//
+
+import SwiftUI
+
+struct StarterView: View {
+    @State private var isStart = false
+    @State private var isEnd = false
+    var body: some View {
+        ZStack {
+            TabView {
+                TimerView()
+                    .tabItem {
+                        Label("Main", systemImage: "clock")
+                    }
+                ResultsView(deleteAction: {})
+                    .tabItem {
+                        Label("Results", systemImage: "list.dash")
+                    }
+                
+                Color.blue
+                    .tabItem {
+                        Label("Settings", systemImage: "gear")
+                    }
+            }
+            Rectangle()
+                .ignoresSafeArea()
+                .foregroundColor(.black)
+                .overlay {
+                    VStack {
+                        Spacer()
+                        Image(isStart ? "logo2": "logo")
+                            .resizable()
+                            .frame(
+                                width: UIScreen.main.bounds.width / 2.5,
+                                height: UIScreen.main.bounds.width / 2.5
+                            )
+                            .cornerRadius(10)
+                            .rotationEffect(Angle(degrees: isStart ? 360 : 0))
+                        Spacer()
+                    }
+                }
+                .opacity(isEnd ? 0 : 1)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation(.spring()) {
+                            isStart.toggle()
+                        }
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            isEnd.toggle()
+                        }
+                    }
+            }
+        }
+    }
+}
+
+struct StarterView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarterView()
+    }
+}
