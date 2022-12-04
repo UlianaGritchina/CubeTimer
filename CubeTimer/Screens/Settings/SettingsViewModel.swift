1c8372359fa2889ddfd3bb5f143defe18ffbecdf
Uliana Gritchina <ugritchina@icloud.com>
2022-12-04 14:09:57 +0300

    SettingsViewModel

---
+++ CubeTimer/Screens/Settings/SettingsViewModel.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+enum Language: String, Codable {
+    case eng = "Eng"
+    case rus = "Rus"
+    case spain = "Es"
+}
+
+class SettingsViewModel: ObservableObject {
+    
+    @Published var selectedLeng: Language = .eng
+    @Published var scramble = UserDefaultsManager.shared.getIsScrsmbrl()
+    
+    init() {
+        setLanguage()
+    }
+    
+    func saveScrubble(_ value: Bool) {
+        UserDefaultsManager.shared.setIsScrambel(scramble)
+    }
+    
+    func setLanguage() {
+        selectedLeng = UserDefaultsManager.shared.getLanguage()
+    }
+    
+    func getMainTitle() -> String {
+        switch selectedLeng {
+        case .eng: return "Settings"
+        case .rus: return "Настройки"
+        case .spain: return "Ajustes"
+        }
+    }
+    
+    func getLengTitle() -> String {
+        switch selectedLeng {
+        case .eng: return "Language"
+        case .rus: return "Язык"
+        case .spain: return "Idioma"
+        }
+    }
+    
+    func getScrubleTitle() -> String {
+        switch selectedLeng {
+        case .eng: return "Show scramble"
+        case .rus: return "Показывать скрамбл"
+        case .spain: return "Mostrar scramble"
+        }
+    }
+    
+}
